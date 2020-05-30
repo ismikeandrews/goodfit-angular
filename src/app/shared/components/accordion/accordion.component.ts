c7143cad870557ac8b5504b6d4e5510cf9d3cbf1
Cyntia Sayuri <cy.sayuri@hotmail.com>
2020-05-30 00:01:19 -0300

    Ajuste do componente accordion - erro na componentização

--- src/app/shared/components/accordion/accordion.component.ts
+++ src/app/shared/components/accordion/accordion.component.ts
@@ -7,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccordionComponent implements OnInit {
 
-  constructor() { }
+  public sobre : any[];
+
+  constructor() {
+    this.sobre = [
+      { icon: 'attach_money', name: 'Salário', text: 'R$ 1800,00' },
+      { icon: 'access_time', name: 'Carga Horária', text: '8H' },
+      { icon: 'format_list_bulleted', name: 'Vagas', text: '3' },
+      { icon: 'description', name: 'Regime Contratação', text: 'Estágio' },
+      { icon: 'location_on', name: 'Endereço', text: 'Av. Lins de Vasconcelos, 1222 - Aclimação', class: 'accordion--content-item__endereco' }
+    ]
+  }
 
   ngOnInit() {
   }
