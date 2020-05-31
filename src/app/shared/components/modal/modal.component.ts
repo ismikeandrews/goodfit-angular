8777468d85487283d9bb080fdccfffd5b55f55c6
Cyntia Sayuri <cy.sayuri@hotmail.com>
2020-05-31 13:23:27 -0300

    Ajuste de layout do accordion

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -15,19 +15,19 @@ export class ModalComponent implements OnInit{
       itemContent:
       [
         { icon: 'attach_money', name: 'Salário', text: 'R$ 1800,00' },
-        { icon: 'access_time', name: 'Carga Horária', text: '8H' },
-        { icon: 'format_list_bulleted', name: 'Vagas', text: '3' },
-        { icon: 'description', name: 'Regime Contratação', text: 'Estágio' },
-        { icon: 'location_on', name: 'Endereço', text: 'Av. Lins de Vasconcelos, 1222 - Aclimação', class: 'accordion--content-item__endereco' }
+        { icon: 'access_time', name: 'Carga Horária', text: '8 horas' },
+        { icon: 'description', name: 'Regime de Contratação', text: 'Estágio' },
+        { icon: 'location_on', name: 'Endereço', text: 'Av. Lins de Vasconcelos, 1222 - Aclimação' },
+        { icon: 'format_list_bulleted', name: 'Vagas Disponíveis', text: '3' },
       ]
     },
     {
       itemType: { name: 'Benefícios' },
       itemContent: 
       [
-        { icon: 'attach_money', name: 'Vale Refeição', text: 'R$ 50,00 (por dia)' },
-        { icon: 'access_time', name: 'Vale Alimentação', text: 'R$ 100,00 (por mês)' },
-        { icon: 'format_list_bulleted', name: 'Vale Transporte', text: 'R$ 20,00 (por dia)' }
+        { icon: 'fastfood', name: 'Vale Refeição', text: 'R$ 50,00 (por dia)' },
+        { icon: 'shopping_cart', name: 'Vale Alimentação', text: 'R$ 100,00 (por mês)' },
+        { icon: 'directions_bus', name: 'Vale Transporte', text: 'R$ 20,00 (por dia)' }
       ]
     }
   ];
