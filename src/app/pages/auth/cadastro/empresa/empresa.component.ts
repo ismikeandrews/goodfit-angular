610d1ae9684c91da97e384596c7f3eb9ff8ddf97
Luis Guilherme Bezerra Bispo <luguilherm@engdb.infra>
2020-10-04 06:45:32 -0300

    Tela de Cadastro Empresa

--- src/app/pages/auth/cadastro/empresa/empresa.component.ts
+++ src/app/pages/auth/cadastro/empresa/empresa.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Empresa } from './../../../../models/empresa.model';
 
 @Component({
   selector: 'app-empresa',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpresaComponent implements OnInit {
 
+  private empresaModel: Empresa = new Empresa();
+
+  @Output() sendEmpresaModel = new EventEmitter<Object>();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  sendModels(){
+    this.sendEmpresaModel.emit(this.empresaModel);
+  }
+
 }
+
