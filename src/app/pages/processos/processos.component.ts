1318cb6aa928149d0625f30d8d3ce5c39570efb8
Cyntia Sayuri <cy.sayuri@hotmail.com>
2020-10-22 22:55:24 -0300

    Processos | Adição dos botões na tabela de status

--- src/app/pages/processos/processos.component.ts
+++ src/app/pages/processos/processos.component.ts
@@ -7,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProcessosComponent implements OnInit {
 
-  constructor() { }
-
+  constructor() {
+  }
+  
   ngOnInit() {
   }
 
